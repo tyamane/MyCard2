b1099ba43a11c613c475d421ccf5b4599f48cfe3
Takao Yamane <borg.tyamane@gmail.com>
2021-07-04 18:53:02 +0900

    deploy

---
+++ precache-manifest.4b5c8929072904a299e177829186879f.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988be98f12b400c41a22b59b82cfeab6",
+    "url": "/MyCard2/assets/icon/favicon.png"
+  },
+  {
+    "revision": "69e6f162abf8c9f65d6d8c73d7813f3e",
+    "url": "/MyCard2/assets/icon/icon.png"
+  },
+  {
+    "revision": "e535ce83da20a4b7719ca3d45195ebd5",
+    "url": "/MyCard2/assets/shapes.svg"
+  },
+  {
+    "revision": "b2c242178289cfe037c9",
+    "url": "/MyCard2/css/app.e32995c9.css"
+  },
+  {
+    "revision": "4da550d3bd98727dcee6",
+    "url": "/MyCard2/css/chunk-42ee8f67.26b9b620.css"
+  },
+  {
+    "revision": "98847ef8fffc24a9cbd2",
+    "url": "/MyCard2/css/chunk-441ccdea.26b9b620.css"
+  },
+  {
+    "revision": "f05e48a13da94aa0ed31",
+    "url": "/MyCard2/css/chunk-6f08c232.26b9b620.css"
+  },
+  {
+    "revision": "9f384ff4666e490941d8",
+    "url": "/MyCard2/css/chunk-vendors.d0ded9bd.css"
+  },
+  {
+    "revision": "d8d03ef8ac564c0eec37cd85b926e046",
+    "url": "/MyCard2/index.html"
+  },
+  {
+    "revision": "b2c242178289cfe037c9",
+    "url": "/MyCard2/js/app.a2887bfe.js"
+  },
+  {
+    "revision": "916995a542a182f113d9",
+    "url": "/MyCard2/js/chunk-02c995b5.c8dee7ab.js"
+  },
+  {
+    "revision": "a9f1696f7e067480bd7a",
+    "url": "/MyCard2/js/chunk-09155df3.e3ac211b.js"
+  },
+  {
+    "revision": "c33ac1675ee0c3d9ba3e",
+    "url": "/MyCard2/js/chunk-09162720.102e7a40.js"
+  },
+  {
+    "revision": "d44aeb33bc6ebfeedadc",
+    "url": "/MyCard2/js/chunk-097e0872.3522b5ab.js"
+  },
+  {
+    "revision": "d38a23c0e380dbc2c768",
+    "url": "/MyCard2/js/chunk-0e93a01c.b0a5975b.js"
+  },
+  {
+    "revision": "e81b88073ae1aa249983",
+    "url": "/MyCard2/js/chunk-1bff95de.1b6757b8.js"
+  },
+  {
+    "revision": "8534cc5992df284a8a10",
+    "url": "/MyCard2/js/chunk-1c4705f4.6dbe2887.js"
+  },
+  {
+    "revision": "fbaa99682929487be06c",
+    "url": "/MyCard2/js/chunk-1dbc15a2.8c41fb29.js"
+  },
+  {
+    "revision": "60ddfde7142271b45259",
+    "url": "/MyCard2/js/chunk-21a76241.9a71d653.js"
+  },
+  {
+    "revision": "0b5fbbef65a956922ebd",
+    "url": "/MyCard2/js/chunk-25d83ca8.e886d2bd.js"
+  },
+  {
+    "revision": "b5b704c84301503db112",
+    "url": "/MyCard2/js/chunk-2d0a463b.580f9170.js"
+  },
+  {
+    "revision": "21ba15a255a6a0478cc7",
+    "url": "/MyCard2/js/chunk-2d0b33e3.2e69defc.js"
+  },
+  {
+    "revision": "f0ac8b87c09e6e4ad1f2",
+    "url": "/MyCard2/js/chunk-2d0b9074.cf6d8bbf.js"
+  },
+  {
+    "revision": "d8ea207e3a9fae89217f",
+    "url": "/MyCard2/js/chunk-2d0b9280.58416f48.js"
+  },
+  {
+    "revision": "564f0792c621c5c04375",
+    "url": "/MyCard2/js/chunk-2d0baac9.ecbd8a2b.js"
+  },
+  {
+    "revision": "f78fd3b2e51a22baae47",
+    "url": "/MyCard2/js/chunk-2d0c073f.017f6960.js"
+  },
+  {
+    "revision": "9d2d53b6b8f97c3d4672",
+    "url": "/MyCard2/js/chunk-2d0c9758.62f099a9.js"
+  },
+  {
+    "revision": "0ad07f107421f9ab12fe",
+    "url": "/MyCard2/js/chunk-2d0d43da.c6672c38.js"
+  },
+  {
+    "revision": "2dccdfe939821adb706d",
+    "url": "/MyCard2/js/chunk-2d0da04a.620f425b.js"
+  },
+  {
+    "revision": "8eee4496a96440b38655",
+    "url": "/MyCard2/js/chunk-2d0e5812.bc3079e1.js"
+  },
+  {
+    "revision": "32b0b3dcf853ab99f765",
+    "url": "/MyCard2/js/chunk-2d213189.2b11f265.js"
+  },
+  {
+    "revision": "43001d3f48887291426f",
+    "url": "/MyCard2/js/chunk-2d217e6a.9d5e5387.js"
+  },
+  {
+    "revision": "dfe50525ff958069dde9",
+    "url": "/MyCard2/js/chunk-2d218068.115cead1.js"
+  },
+  {
+    "revision": "fc7b2f95d49c48c8414a",
+    "url": "/MyCard2/js/chunk-2d21da73.56e20878.js"
+  },
+  {
+    "revision": "a49fbb56d12037370dd1",
+    "url": "/MyCard2/js/chunk-2d2297f7.78980670.js"
+  },
+  {
+    "revision": "49380faa8efc50a7cd65",
+    "url": "/MyCard2/js/chunk-2d2304d2.72233652.js"
+  },
+  {
+    "revision": "fe1929d6f0985f65c532",
+    "url": "/MyCard2/js/chunk-2d2376e6.a05b70ee.js"
+  },
+  {
+    "revision": "08c0c20d063c3f8bae82",
+    "url": "/MyCard2/js/chunk-2d237b00.037b5772.js"
+  },
+  {
+    "revision": "2e86d9d39beb6b73dbec",
+    "url": "/MyCard2/js/chunk-30dd9750.6b7d6eb0.js"
+  },
+  {
+    "revision": "575d4429d28913dbfee0",
+    "url": "/MyCard2/js/chunk-37f2300e.cc3c45b7.js"
+  },
+  {
+    "revision": "b2ba2273c82af3241226",
+    "url": "/MyCard2/js/chunk-3ed29de3.9e68e146.js"
+  },
+  {
+    "revision": "efb4907469ba6d4e25b3",
+    "url": "/MyCard2/js/chunk-41c0f7a4.5f0941d1.js"
+  },
+  {
+    "revision": "d5724ed7d66a31c83d13",
+    "url": "/MyCard2/js/chunk-426f2f16.9f46eefa.js"
+  },
+  {
+    "revision": "4da550d3bd98727dcee6",
+    "url": "/MyCard2/js/chunk-42ee8f67.36f2b613.js"
+  },
+  {
+    "revision": "98847ef8fffc24a9cbd2",
+    "url": "/MyCard2/js/chunk-441ccdea.dfd02fa0.js"
+  },
+  {
+    "revision": "f14e7f1aa4c8d1a4d6a4",
+    "url": "/MyCard2/js/chunk-462c869e.0ea6ae2c.js"
+  },
+  {
+    "revision": "b303ebea99c66d69f54e",
+    "url": "/MyCard2/js/chunk-47245a33.b9e6a322.js"
+  },
+  {
+    "revision": "e7715be253dd17263940",
+    "url": "/MyCard2/js/chunk-4739acd0.b82826b2.js"
+  },
+  {
+    "revision": "81ae71805e61fce09362",
+    "url": "/MyCard2/js/chunk-53b1137a.40a5f38c.js"
+  },
+  {
+    "revision": "ccfba1ffdefbac2b9f84",
+    "url": "/MyCard2/js/chunk-541ea42d.63c37d24.js"
+  },
+  {
+    "revision": "1117ae8f4832db3b1c05",
+    "url": "/MyCard2/js/chunk-54bbd082.aa2ffdeb.js"
+  },
+  {
+    "revision": "c5a12c05e4fcbf8ecc05",
+    "url": "/MyCard2/js/chunk-59d4c87c.9a2960ba.js"
+  },
+  {
+    "revision": "2da0a95be4ff461eda97",
+    "url": "/MyCard2/js/chunk-5a8ac886.f8dd7eae.js"
+  },
+  {
+    "revision": "9147a72bc499e6a3eb6b",
+    "url": "/MyCard2/js/chunk-5ff96a24.bc2c1407.js"
+  },
+  {
+    "revision": "b4168046f096b9c1aa84",
+    "url": "/MyCard2/js/chunk-69eb8776.8f434896.js"
+  },
+  {
+    "revision": "f215811c4c408e27f968",
+    "url": "/MyCard2/js/chunk-6d375f34.8b250ad4.js"
+  },
+  {
+    "revision": "f05e48a13da94aa0ed31",
+    "url": "/MyCard2/js/chunk-6f08c232.b2fa47ed.js"
+  },
+  {
+    "revision": "07d1a7d7c9c14145b069",
+    "url": "/MyCard2/js/chunk-7548ca2d.f50c2a89.js"
+  },
+  {
+    "revision": "3901bb5ac6a64486c6c8",
+    "url": "/MyCard2/js/chunk-79f1dac7.4481f814.js"
+  },
+  {
+    "revision": "3d61bda538780760ae08",
+    "url": "/MyCard2/js/chunk-858cd918.54b9acef.js"
+  },
+  {
+    "revision": "c2157c9e7c7c822ee62b",
+    "url": "/MyCard2/js/chunk-8bb7f81e.4465b78a.js"
+  },
+  {
+    "revision": "2f459fbd3853a20e231b",
+    "url": "/MyCard2/js/chunk-a9395c36.8777d2b5.js"
+  },
+  {
+    "revision": "860e7e4557a42cc2dd2e",
+    "url": "/MyCard2/js/chunk-d0e8aa0a.ef2f5d14.js"
+  },
+  {
+    "revision": "d38edb9f053d3942419b",
+    "url": "/MyCard2/js/chunk-d8413eac.9e3a3351.js"
+  },
+  {
+    "revision": "ebcfbe9d137e7c95bd9c",
+    "url": "/MyCard2/js/chunk-e5275ddc.fb4bde6d.js"
+  },
+  {
+    "revision": "b7c2dffece48262fe4e8",
+    "url": "/MyCard2/js/chunk-eaa2b130.700c0fb3.js"
+  },
+  {
+    "revision": "9f384ff4666e490941d8",
+    "url": "/MyCard2/js/chunk-vendors.ca304d87.js"
+  },
+  {
+    "revision": "c6ef1925cf7e7731df86",
+    "url": "/MyCard2/js/polyfills-core-js.12be3461.js"
+  },
+  {
+    "revision": "f8a0775bfceb8f823175",
+    "url": "/MyCard2/js/polyfills-dom.662760da.js"
+  },
+  {
+    "revision": "648fd8ce0e8350347c524915cb5b0b3f",
+    "url": "/MyCard2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MyCard2/robots.txt"
+  }
+]);
