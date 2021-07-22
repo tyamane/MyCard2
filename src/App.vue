87b50b2724b71c7a2c8dc39ede2d88913c3ac47e
Takao Yamane <borg.tyamane@gmail.com>
2021-07-22 21:06:08 +0900

    メニュー追加

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,77 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <ion-menu side="start" content-id="main-content">
+      <ion-header>
+        <ion-toolbar translucent>
+          <ion-title>Menu</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <ion-list>
+          <ion-item>
+            <ion-icon name="hert" slot="start"></ion-icon>
+            <ion-label>Inbox</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-icon name="paper-plane" slot="start"></ion-icon>
+            <ion-label>Outbox</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-icon name="heart" slot="start"></ion-icon>
+            <ion-label>Favorites</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-icon name="archive" slot="start"></ion-icon>
+            <ion-label>Archived</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-icon name="trash" slot="start"></ion-icon>
+            <ion-label>Trash</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-icon name="warning" slot="start"></ion-icon>
+            <ion-label>Spam</ion-label>
+          </ion-item>
+        </ion-list>
+      </ion-content>
+    </ion-menu>
+    <ion-router-outlet id="main-content" />
   </ion-app>
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { 
+  IonApp, 
+  IonRouterOutlet, 
+  IonContent, 
+  IonHeader, 
+  IonItem, 
+  IonList, 
+  IonMenu, 
+  IonTitle, 
+  IonToolbar,
+  IonIcon,
+  IonButtons
+} from '@ionic/vue';
+import { heart } from 'ionicons/icons';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
-    IonRouterOutlet
+    IonRouterOutlet,
+    IonContent, 
+    IonHeader, 
+    IonItem, 
+    IonList, 
+    IonMenu, 
+    IonTitle, 
+    IonToolbar,
+    IonIcon
+  },
+  setup(){
+    return { heart }
   }
 });
 </script>
--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -1,7 +1,11 @@
 <template>
+
   <ion-page>
     <ion-header>
       <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
         <ion-title>Tab 1</ion-title>
       </ion-toolbar>
     </ion-header>
@@ -11,18 +15,19 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
       <ExploreContainer name="Tab 1 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonMenuButton} from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 
-export default  {
+export default {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonMenuButton },
+  methods: {
+  }
 }
 </script>
